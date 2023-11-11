39c03efbf745141170036f25249c08952e0986b7
Davi Brito <125412383+DavidBrito32@users.noreply.github.com>
2023-11-11 14:44:38 -0300

    Envio Inicial

---
+++ src/routes/Router.jsx
@@ -0,0 +1,28 @@
+import { Routes, Route, useLocation } from "react-router-dom";
+import LayoutPage from "../layouts";
+import HomePage from "../pages/home";
+import PokedexPage from "../pages/pokedex";
+import ErrorPage from "../pages/error";
+import PokeDetails from "../pages/pokeDetails";
+import { AnimatePresence } from "framer-motion";
+
+const Router = () => {
+  const location = useLocation();
+
+  return (
+    <>
+      <AnimatePresence mode="wait">
+        <Routes key={location.pathname} location={location}>
+          <Route path="/" element={<LayoutPage />}>
+            <Route index element={<HomePage />} />
+            <Route path="/pokedex" element={<PokedexPage />} />
+            <Route path="/pokemon/:name" element={<PokeDetails />} />
+            <Route path="*" element={<ErrorPage />} />
+          </Route>
+        </Routes>
+      </AnimatePresence>
+    </>
+  );
+};
+
+export default Router;
